5148176e92d8a93e86842bcdefa55e1716625d6c
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-10-14 20:30:14 +0300

    додав інші сторінки. Використав метод createReadStream і pipe

--- www/index.js
+++ www/index.js
@@ -24,12 +24,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
     });
-    fs.readFile("./www/index.html", "utf-8", (err, data) => {
-        res.end(
-            data
-        );
-    })
-   
+
+    if (req.url == "/") fs.createReadStream('./www/index.html').pipe(res)
+    else if (req.url === "/about") fs.createReadStream("./www/about.html").pipe(res);
+    else fs.createReadStream("./www/error.html").pipe(res)
 });
 
 const PORT = 3000;
