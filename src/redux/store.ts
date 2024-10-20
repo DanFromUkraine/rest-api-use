7039061a89a7e1804bb6e5947fea263255154a6f
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-10-20 21:44:13 +0300

    Greatgit add . I a little changed my paradigm to the problem. Before I wanted to emulate all redux store functionaly creating copies just for tests, but now I am using existing components to change my store state and I'm using components to view it

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,16 +2,10 @@ import { configureStore, Tuple } from "@reduxjs/toolkit";
 import rootReducer from "./mainSlice";
 import { crashReporter } from "./errorHandler";
 
-export const store = createStore();
-
-export function createStore() {
-  const store = configureStore({
-    reducer: rootReducer,
-    middleware: () => new Tuple(crashReporter),
-  });
-
-  return store;
-}
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => new Tuple(crashReporter),
+})
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- tests/components/ActionDescription.test.tsx
+++ tests/components/ActionDescription.test.tsx
@@ -1,28 +1,40 @@
 import { render, screen } from "@testing-library/react";
-import { ActionDescription } from "../../src/components";
+import { ActionDescription, GetBtn } from "../../src/components";
 import { describe, it, expect } from "vitest";
 import React from "react";
-import { createStore } from "../../src/redux/store";
 import { Provider } from "react-redux";
+import { ActionDescriptionTexts } from "../../src/constants";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
+import { store } from "../../src/redux/store";
 
 describe("ActionDescription", () => {
   function renderFC() {
-    const store = createStore();
-
     const { container } = render(
-      <Provider store={store}>
-        <ActionDescription />
-      </Provider>
+      <>
+        <Provider store={store}>
+          <ActionDescription />
+          <GetBtn />
+        </Provider>
+      </>
     );
 
-    return [store, container];
+    vi.mock("../redux/store", () => store);
+
+    return container;
   }
 
-  it("should render component, show description to action, if it is chosen, and oposit. Should show error if it exists", () => {
-    const [store, container] = renderFC();
+  it("should render components GetButton and ActionDescription, show description to action, if it is chosen, and oposit. Should show error if it exists", async () => {
+    renderFC();
 
-    
+    const getButton = screen.getByRole("button");
+    const description = screen.getByRole("paragraph");
 
-    expect(container).toBeInTheDocument();
+    expect(description).toHaveTextContent(ActionDescriptionTexts[""]);
+
+    const user = userEvent.setup();
+    await user.click(getButton);
+
+    expect(description).toHaveTextContent(ActionDescriptionTexts.get);
   });
 });
